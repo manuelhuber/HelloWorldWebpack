1b761588e8609915a5eddfa7c3f44f5ae6062c41
Manuel Huber <manuel.huber@qaware.de>
2015-10-05 17:20:05 +0200

    Added Gulp Tasks for Coverage: Run Webpack to compile all the .ts files to 1 .js bundle Run Karma for Test Coverage of the Bundle Run remap-Istanbul for remapping the Coverage back to the Original .ts files Run the lcov Hack to fixed the messed up file Paths in the resulting lcov (WARNING: filthy!)

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,21 @@
 var gulp = require('gulp');
+var del = require('del');
 var webpack = require('webpack');
+var karma = require('karma');
+var remapIstanbul = require('remap-istanbul/lib/gulpRemapIstanbul');
+var dirtyHack = require('./remapLcovHack.js');
 
+var karmaCoverageOut = __dirname + "\\karmaCoverage";
+var webpackOut = __dirname + "\\target";
+var remapIstanbulOut = "remapIstanbul\\remappedLcov.info";
+var dirtyHackOut = "remapIstanbul\\fixedLcov.info";
 
-gulp.task('default', function () {
-    // place code for your default task here
+gulp.task('clean', function (callback) {
+    del([webpackOut, karmaCoverageOut, remapIstanbulOut, dirtyHackOut]);
+    callback();
+});
+
+gulp.task('coverage-webpack', ['clean'], function (callback) {
     webpack({
         resolve: {
             extensions: ['', '.ts', '.webpack.js', '.web.js', '.js']
@@ -14,17 +26,43 @@ gulp.task('default', function () {
             spec: "./test/HelloWorldSpec"
         },
         output: {
-            path: __dirname + "\\target",
-            filename: "[name]bundle.js"
+            path: webpackOut,
+            filename: "[name]bundle.js",
+            devtoolModuleFilenameTemplate: "::delete::[absolute-resource-path]",
         },
 
         module: {
             loaders: [
-                { test: /\.ts$/, loader: 'source-map-loader!ts-loader'}
+                { test: /\.ts$/, loader: 'source-map-loader!ts-loader' }
             ]
         }
-    }, function(err, stats) {
-        if(err) throw new gutil.PluginError("webpack", err);
+    }, function (err, stats) {
+        if (err) throw new gutil.PluginError("webpack", err);
+        callback();
     });
+
 });
 
+gulp.task('coverage-karma', ['coverage-webpack'], function (callback) {
+    new karma.Server({
+        configFile: __dirname + '/karma.conf.js',
+        singleRun: true
+    }, callback).start();
+
+})
+
+gulp.task('coverage-remap-istanbul', ['coverage-karma'], function (callback) {
+    return gulp.src('karmaCoverage/report-json/coverage-final.json')
+        .pipe(remapIstanbul({
+            reports: {
+                'lcovonly': remapIstanbulOut
+            }
+        }));
+});
+
+gulp.task('coverage-lcov-hack', ['coverage-remap-istanbul'], function () {
+    dirtyHack.lcovHack(remapIstanbulOut, dirtyHackOut);
+
+});
+
+gulp.task('coverage', ['coverage-lcov-hack']);
--- remapLcovHack.js
+++ remapLcovHack.js
@@ -2,17 +2,14 @@
  * Created by m.huber on 05/10/2015.
  */
 
-var fs = require('fs');
+exports.lcovHack = function (lcovFilePath, outputPath) {
 
-var lcovFilePath = process.argv[2]
+    var fs = require('fs');
 
-var data = fs.readFileSync(lcovFilePath.toString(), 'utf-8');
+    var data = fs.readFileSync("./remapIstanbul/remappedLcov.info", 'utf-8');
 
-var newValue = data.replace(/SF:(.*?)::delete::/g, 'SF:');
+    var newValue = data.replace(/SF:(.*?)::delete::/g, 'SF:');
 
-fs.writeFileSync('lcovHacked.info', newValue, 'utf-8');
+    fs.writeFileSync(outputPath, newValue, 'utf-8');
 
-console.log('readFileSync complete');
-
-
-
+}
